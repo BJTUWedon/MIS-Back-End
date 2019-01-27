9038b5e667a8d54e9315192d1883a576c01fb1bb
Wedon <604543216@qq.com>
2019-01-27 12:52:04 -0500

    1

--- OPM_Project/settings.py
+++ OPM_Project/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': 'OPMdb',
         'USER': 'root',
         'PASSWORD': '888888',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
