b12eec1073e17ecd2c17d345df2ead9f86876de5
Wedon <604543216@qq.com>
2019-02-09 22:22:49 -0500

    1

--- Project/views.py
+++ Project/views.py
@@ -534,8 +534,10 @@ def uploadFile(request):
                         f.write(ffile)
                 if address == '.avi':
                     convert_video(src,hash_code(name)+'.mp4')
-                    # os.remove(src)
-                    src = hash_code(name)+'.mp4'
+                    os.remove(src)
+                    newsrc = hash_code(name)+'.mp4'
+                    type = 'mp4'
+                    File.objects.create(filename=title, type=type, content=content,createDate=datetime.datetime.now(), src=r"http://lvmaozi.info:9999/"+newsrc)#我认为下面还要返回id
                 File.objects.filter(id=id).update(filename=title, type=type, content=content,createDate=datetime.datetime.now(),src=r"http://lvmaozi.info:9999/"+src)
                 Data = {"title": title, "id": id}
         except Exception as e:
