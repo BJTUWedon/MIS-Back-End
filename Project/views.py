cfe3bbf1b1a5e3706556ee62bb79eb3e1824a3bd
Wedon <604543216@qq.com>
2019-02-09 22:11:58 -0500

    1

--- Project/views.py
+++ Project/views.py
@@ -520,7 +520,7 @@ def uploadFile(request):
                 if address == '.avi':
                     convert_video(src,hash_code(name)+'.mp4')
                     os.remove(src)
-                    src = hash_code(name)+'.mp4'            
+                    src = hash_code(name)+'.mp4'
                 File.objects.create(filename=title, type=type, content=content,createDate=datetime.datetime.now(), src=r"http://lvmaozi.info:9999/"+src)#我认为下面还要返回id
 
                 lastFile = File.objects.order_by("-createDate")[0:1].get()
@@ -533,6 +533,8 @@ def uploadFile(request):
                         f.write(ffile)
                 if address == '.avi':
                     convert_video(src,hash_code(name)+'.mp4')
+                    os.remove(src)
+                    src = hash_code(name)+'.mp4'    
                 File.objects.filter(id=id).update(filename=title, type=type, content=content,createDate=datetime.datetime.now(),src=r"http://lvmaozi.info:9999/"+src)
                 Data = {"title": title, "id": id}
         except Exception as e:
