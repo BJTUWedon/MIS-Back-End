c5b175381ed899c4fe6f162a739980a01d319926
Wedon <604543216@qq.com>
2019-02-13 23:48:59 -0500

    1

--- Project/views.py
+++ Project/views.py
@@ -705,7 +705,7 @@ def getFile(request):
                     time = FileList.time
                     Data = {"id": id, "title": title, "content": content, "src": src, "createDate": createDate,"type":type,
                             "authUserList": authUserList,"limit":time}
-            if (User.objects.get(id=userid).isManager == True): #即使没有权限也能访问和编辑
+            if (User.objects.get(id=userid).isManager == True):
                 try:
                     FileList = File_User.objects.get(username_id=userid, filename_id=id)
                 except:
@@ -714,7 +714,6 @@ def getFile(request):
                     try:
                         thisUserFile = File_User.objects.get(filename_id=id, username_id=userid)
                         limit = thisUserFile.time
-                        print(type(limit))
                         Fileinfo = File_User.objects.filter(filename_id=id)
                         if isinstance(Fileinfo, Iterable) == True:
                             for authlist in Fileinfo:
